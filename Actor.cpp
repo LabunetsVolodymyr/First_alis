1d65b7ab870dd1fc0d7f265744c9d20722609b88
Vova Labunets <labunetsvolodymyr1@gmail.com>
2025-05-24 21:16:04 +0300

    Implement Actor: constructors, play() method, and showInfo()

--- Actor.cpp
+++ Actor.cpp
@@ -5,10 +5,8 @@
 #include "Actor.h"
 
 Actor::Actor() : fullName("Unknown Actor"), age(30) {}
-
-Actor::Actor(const std::string& name, int age) : fullName(name), age(age) {}
-
-Actor::Actor(const std::string& name) : Actor(name, 35) {}
+Actor::Actor(std::string name, int age) : fullName(name), age(age) {}
+Actor::Actor(std::string name) : Actor(name, 35) {}
 
 Actor::~Actor() {
     std::cout << "Actor \"" << fullName << "\" deleted\n";
@@ -17,4 +15,9 @@ Actor::~Actor() {
 void Actor::showInfo() const {
     std::cout << "Actor: " << fullName << ", Age: " << age << std::endl;
 }
+
+void Actor::play() const {
+    std::cout << "Actor " << fullName << " is performing a scene." << std::endl;
+}
+
 
